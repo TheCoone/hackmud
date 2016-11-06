4fe2049c5b00c49cf8d7f616a760096e4e94964c
TheCoone <rene.grobbee@gmail.com>
2016-11-06 14:49:36 +0100

    Added tools.js for future use

---
+++ tools.js
@@ -0,0 +1,22 @@
+function (c,a) { // {p:""}
+
+    // personal tool script
+
+
+  switch (p) {
+    default:
+    case info:
+      return "This will show info of this script."
+    case bank:
+      return "This will bank all GC into the alt account."
+    case transfer:
+      return "This will show a bankstatement"
+    case xfer:
+      return "This will Transfer an item to them alt acccount."
+    case clean:
+      return "This will cleanup all Upgrades."
+    case
+
+  }
+
+}
