6596d071a38777e10ed889079e10006d1fa183b0
TheCoone <rene.grobbee@gmail.com>
2016-11-09 11:58:36 +0100

    Update tools.js
    Changed some code so it displays the help when not using an curlybraces

--- tools.js
+++ tools.js
@@ -1,21 +1,26 @@
-function (c,a) { // {p:""}
-
-    // personal tool script
-
+function (context,args) { // {p:""}
+     // personal tool script
+  if(args == null) { p = "info"} else { var p = args.p  }
+  var name = args.name
 
   switch (p) {
+    case "info":
     default:
-    case info:
-      return "This will show info of this script."
-    case bank:
-      return "This will bank all GC into the alt account."
-    case transfer:
+      return "Please use the following !commands! with {page:\" cmd \"}\n\n" +
+             "+bank+     - Will bank all GC into the alt account.\n" +
+             "+xfer+     - Will transfer all Upgrades to the alt.\n" +
+             "+transfer+ - Will show the bankstatements.\n" +
+             "+clean+    - This will cleanup (destroy) all Upgrades.\n" +
+             "+panic+    - Will completly clean out the account."
+    case "bank":
+      return "This will bank all GC into the alt account with the name:" + name +""
+    case "transfer":
       return "This will show a bankstatement"
-    case xfer:
+    case "xfer":
       return "This will Transfer an item to them alt acccount."
-    case clean:
+    case "clean":
       return "This will cleanup (destroy) all Upgrades."
-    case panic:
+    case "panic":
       return "This will be the panic button to burn the user"
 
   }
